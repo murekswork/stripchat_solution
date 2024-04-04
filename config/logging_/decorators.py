e109fe508094e270753b1e678c4e36ba563bd68d
murekswork <murekswork@gmail.com>
2024-04-05 00:42:30 +0300

    final result

--- config/logging_/decorators.py
+++ config/logging_/decorators.py
@@ -3,16 +3,17 @@ import logging
 
 
 def log_exception(logger: logging.Logger):
+    """Decorator to log all actions and exceptions"""
     def decorator(func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
             try:
                 result = func(*args, **kwargs)
-                logger.debug("{} action successfully completed".format(func.__name__))
+                logger.warning("{} action successfully completed".format(func.__name__))
                 return result
             except Exception as exc:
                 logger.warning("{} exception occured: {} ".format(func.__name__, exc))
-                raise Exception("{} exception occured: {} ".format(func, exc))
+                raise Exception("{} exception occured: {} ".format(func.__name__, exc))
 
         return wrapper
 
--- config/solution/services/parsing_service.py
+++ config/solution/services/parsing_service.py
@@ -2,12 +2,11 @@ import logging
 import threading
 import time
 
+from accounts.models import StripChatUser
 from django.conf import settings
+from logging_.decorators import log_exception
 from playwright.sync_api import sync_playwright, Page, Frame, ElementHandle
 from twocaptcha import TwoCaptcha
-
-from accounts.models import StripChatUser
-from logging_.decorators import log_exception
 
 parsing_logger = logging.getLogger(name="StripChatService logger")
 captcha_logger = logging.getLogger(name="CaptchaSolver logger")
@@ -17,10 +16,11 @@ class StripChatLoggingService:
     def __init__(self, pw):
         self.playwright = pw
         self.page: Page
-        self.captcha_link: str | None = None
+        self.captcha_link: str
 
     @log_exception(parsing_logger)
     def get_browser_page(self) -> Page:
+        """Method to create a browser and the page"""
         browser = self.playwright.webkit.launch(headless=False)
         context = browser.new_context(**self.playwright.devices["iPhone 6"])
         page = context.new_page()
@@ -30,6 +30,7 @@ class StripChatLoggingService:
 
     @log_exception(parsing_logger)
     def get_captcha_content_frame(self) -> Frame:
+        """Method to select captcha content frame and return it"""
         captcha_frame_selector = self.page.wait_for_selector(
             "iframe[src*='recaptcha/api2']"
         )
@@ -48,6 +49,7 @@ class StripChatLoggingService:
 
     @log_exception(parsing_logger)
     def input_account_credentials(self, username: str, password: str) -> None:
+        """Method to enter provided uname and pass to login inputs"""
         username = username
         password = password
         self.page.wait_for_selector("input[id='login_login_or_email']").fill(username)
@@ -55,11 +57,13 @@ class StripChatLoggingService:
 
     @log_exception(parsing_logger)
     def input_captcha_code(self, code: str) -> None:
+        """Method to enter provided captcha code into hidden captcha response input"""
         captcha_input = self.page.locator("#g-recaptcha-response")
         captcha_input.evaluate(f'(element) => {{ element.value = "{code}"; }}')
 
     def call_captcha_callback(self, captcha_code: str) -> None:
-        letters = "QWERTYUIOPASDFGHJKLZXCVBNM"
+        """Method to iterate over all site's clients and try to call 'callback' method"""
+        letters = "XQWERTYUIOPASDFGHJKLZCVBNM"
         for letter in letters:
             try:
                 resp = self.page.evaluate(
@@ -85,6 +89,7 @@ class StripChatLoggingService:
 
     @log_exception(parsing_logger)
     def search(self, username: str) -> None:
+        """Method to enter required string in site's search field and click search button """
         search_button = self.page.get_by_role("search").click()
         try:
             search_window = self.page.get_by_placeholder(
@@ -106,6 +111,7 @@ class CaptchaSolver:
 
     @log_exception(captcha_logger)
     def solve_captcha(self) -> str:
+        """Method to send site's key and url to captcha solving service"""
         captcha_code = self.solver.recaptcha(
             version="v2", sitekey=self.site_key, url="https://stripchat.global/login"
         )
@@ -113,6 +119,7 @@ class CaptchaSolver:
 
 
 def run_in_thread(user: StripChatUser, search_username: str):
+    """Function to call main function in separate thread with daemon mode"""
     thread = threading.Thread(
         target=solve_captcha_and_login,
         args=(
@@ -125,8 +132,15 @@ def run_in_thread(user: StripChatUser, search_username: str):
 
 
 def solve_captcha_and_login(
-    user: StripChatUser, search_username: str, retries: int = 0
+        user: StripChatUser, search_username: str, retries: int = 0
 ):
+    """
+    This function attempts to solve a captcha, login to a StripChat account,
+     and search for a specified username. If attempt fails,
+     it retries up to 5 times (by default).
+     The function takes a StripChat user object, a search username,
+     and an optional number of retries as parameters.
+    """
     try:
         with sync_playwright() as pw:
             parser = StripChatLoggingService(pw)
@@ -137,7 +151,7 @@ def solve_captcha_and_login(
             solver = CaptchaSolver(site_key, settings.CAPTCHA_SOLVER_API_KEY)
             captcha_code = solver.solve_captcha()
 
-            parser.input_captcha_code(captcha_frame, captcha_code)
+            parser.input_captcha_code(captcha_code)
             parser.input_account_credentials(user.username, user.password)
             parser.call_captcha_callback(captcha_code)
 
